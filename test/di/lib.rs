712d8cc2ce5afcb38a9330178d12b3de36274dbd
Chris Martinez <chmrtnz@amazon.com>
2023-02-04 13:36:05 -0800

    Update marco to add validation

--- test/di/lib.rs
+++ test/di/lib.rs
@@ -1,5 +1,10 @@
 #![cfg(test)]
 
+// since this is a test crate, the test configuration needs to be
+// specified in order to expand macros
+//
+// RUSTFLAGS='--cfg test' cargo expand
+
 mod traits;
 mod structs;
 mod containers;
