9bb3fc31c5b79a11d15d2ce301c22db1214ff063
Chris Martinez <chmrtnz@amazon.com>
2023-02-08 15:28:35 -0800

    Add lazy-initialization support

--- src/di/lib.rs
+++ src/di/lib.rs
@@ -1,10 +1,10 @@
 #![doc = include_str!("README.md")]
 
-mod r#type;
 mod collection;
 mod dependency;
 mod descriptor;
 mod provider;
+mod r#type;
 mod validation;
 
 #[cfg(feature = "builder")]
@@ -13,14 +13,17 @@ mod builder;
 #[cfg(feature = "inject")]
 mod inject;
 
+#[cfg(feature = "lazy")]
+mod lazy_init;
+
 #[cfg(test)]
 mod test;
 
-pub use r#type::*;
 pub use collection::*;
 pub use dependency::*;
 pub use descriptor::*;
 pub use provider::*;
+pub use r#type::*;
 pub use validation::*;
 
 #[cfg(feature = "builder")]
@@ -31,3 +34,10 @@ pub use inject::*;
 
 #[cfg(feature = "inject")]
 pub use di_macros::{inject, injectable};
+
+/// Contains support for lazy service resolution.
+#[cfg(feature = "lazy")]
+pub mod lazy {
+    use super::*;
+    pub use lazy_init::{empty, exactly_one, missing, zero_or_more, zero_or_one, Lazy};
+}
