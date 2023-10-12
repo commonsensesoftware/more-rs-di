cb97f005f39c46c92f7163cc282c7c831c700268
Chris Martinez <sydefekt@hotmail.com>
2023-10-12 09:09:58 -0700

    Support keyed type

--- src/di/type.rs
+++ src/di/type.rs
@@ -6,55 +6,78 @@ use std::hash::{Hash, Hasher};
 /// Represents a type.
 #[derive(Clone, Debug, Eq)]
 pub struct Type {
-	id: u64,
-	name: String,
+    id: u64,
+    name: String,
+    key: Option<String>,
 }
 
 impl Type {
-	/// Initializes a new instance of a type.
-	pub fn of<T: Any + ?Sized>() -> Self {
-		let name = type_name::<T>().to_string();
-		let mut hasher = DefaultHasher::new();
+    /// Initializes a new instance of a type.
+    pub fn of<T: Any + ?Sized>() -> Self {
+        Type::new(type_name::<T>().to_string(), None)
+    }
 
-		name.hash(&mut hasher);
+    /// Initializes a new instance of a type based on another type as a key.
+    pub fn keyed<TKey, TType: Any + ?Sized>() -> Self {
+        Type::new(
+            type_name::<TType>().to_string(),
+            Some(type_name::<TKey>().to_string()),
+        )
+    }
 
-		Self {
-			id: hasher.finish(),
-			name,
-		}
-	}
+    fn new(name: String, key: Option<String>) -> Self {
+        let mut hasher = DefaultHasher::new();
 
-	/// Gets the type identifier.
-	pub fn id(&self) -> u64 {
-		self.id
-	}
+        name.hash(&mut hasher);
 
-	/// Gets the type name.
-	pub fn name(&self) -> &str {
-		&self.name
-	}
+        if let Some(ref val) = key {
+            val.hash(&mut hasher);
+        }
+
+        Self {
+            id: hasher.finish(),
+            name,
+            key,
+        }
+    }
+
+    /// Gets the type identifier.
+    pub fn id(&self) -> u64 {
+        self.id
+    }
+
+    /// Gets the type name.
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// Deconstructs the specified type into its name component
+    /// and key component, if it is defined.
+    pub fn deconstruct(t: &Type) -> (&str, Option<&str>) {
+        (&t.name, t.key.as_deref())
+    }
 }
 
 impl PartialEq<Type> for Type {
-	fn eq(&self, other: &Self) -> bool {
-		self.id == other.id
-	}
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
 }
 
 impl PartialEq<Type> for &Type {
-	fn eq(&self, other: &Type) -> bool {
-		self.id == other.id
-	}
+    fn eq(&self, other: &Type) -> bool {
+        self.id == other.id
+    }
 }
 
 impl Hash for Type {
-	fn hash<H: Hasher>(&self, state: &mut H) {
-		self.id.hash(state);
-	}
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
+    }
 }
 
 impl Display for Type {
-	fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatResult {
-		formatter.write_str(&self.name)
-	}
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> FormatResult {
+        formatter.write_str(&self.name)
+    }
 }
