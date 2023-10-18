7c469f44643c710b68bddbcc8c024e890939cad1
Chris Martinez <sydefekt@hotmail.com>
2023-10-18 09:34:53 -0700

    Refactor trait with builder for mutability and key

---
+++ src/di/activator.rs
@@ -0,0 +1,65 @@
+use crate::{ServiceFactory, ServiceProvider, ServiceRef, ServiceRefMut, Type};
+use std::{any::Any, sync::Mutex};
+
+/// Represents an activator for a service instance.
+pub struct Activator {
+    service_type: Type,
+    service_type_mut: Type,
+    implementation_type: Type,
+    factory: ServiceRef<ServiceFactory>,
+    factory_mut: ServiceRef<ServiceFactory>,
+    mutable: bool,
+}
+
+impl Activator {
+    /// Gets the [service type](struct.Type.html) associated with the service descriptor.
+    pub fn service_type(&self) -> &Type {
+        if self.mutable {
+            &self.service_type_mut
+        } else {
+            &self.service_type
+        }
+    }
+
+    /// Gets the [implementation type](struct.Type.html) associated with the service descriptor.
+    pub fn implementation_type(&self) -> &Type {
+        &self.implementation_type
+    }
+
+    /// Sets a value indicating whether the activated instance should be mutable.
+    pub fn as_mut(&mut self) {
+        self.mutable = true;
+    }
+
+    /// Gets the factory method the activator represents.
+    pub fn factory(&self) -> ServiceRef<ServiceFactory> {
+        if self.mutable {
+            self.factory_mut.clone()
+        } else {
+            self.factory.clone()
+        }
+    }
+
+    /// Creates a new activator using the specified factory methods to instantiate the service.
+    ///
+    /// # Arguments
+    ///
+    /// * `factory` - The factory method used to create a service instance
+    /// * `factory_mut` - The factory method used to create a mutable service instance
+    pub fn new<TSvc, TImpl, F1, F2>(factory: F1, factory_mut: F2) -> Self
+    where
+        TSvc: Any + ?Sized,
+        TImpl: 'static,
+        F1: Fn(&ServiceProvider) -> ServiceRef<TSvc> + 'static,
+        F2: Fn(&ServiceProvider) -> ServiceRefMut<TSvc> + 'static,
+    {
+        Self {
+            service_type: Type::of::<TSvc>(),
+            service_type_mut: Type::of::<Mutex<TSvc>>(),
+            implementation_type: Type::of::<TImpl>(),
+            factory: ServiceRef::new(move |sp| ServiceRef::new(factory(sp))),
+            factory_mut: ServiceRef::new(move |sp| ServiceRef::new(factory_mut(sp))),
+            mutable: false,
+        }
+    }
+}
---
+++ src/di/inject_builder.rs
@@ -0,0 +1,78 @@
+use crate::{Activator, ServiceDependency, Type, ServiceLifetime, ServiceDescriptor};
+use spin::Once;
+
+/// Represents the builder for an injected type.
+pub struct InjectBuilder {
+    activator: Activator,
+    lifetime: ServiceLifetime,
+    key_type: Option<Type>,
+    dependencies: Vec<ServiceDependency>,
+}
+
+impl InjectBuilder {
+    /// Initializes a new builder.
+    ///
+    /// # Arguments
+    ///
+    /// * `activator` - The [activator](struct.Activator.html) used to activate the service
+    /// * `lifetime` - The [lifetime](enum.ServiceLifetime.html) of the service
+    pub fn new(activator: Activator, lifetime: ServiceLifetime) -> Self {
+        Self {
+            activator,
+            lifetime,
+            key_type: None,
+            dependencies: Vec::default(),
+        }
+    }
+
+    /// Defines a dependency used by the service.
+    ///
+    /// # Arguments
+    ///
+    /// * `dependency` - The [dependency](struct.ServiceDependency.html) associated with the services
+    pub fn depends_on(mut self, dependency: ServiceDependency) -> Self {
+        if !self.dependencies.contains(&dependency) {
+            self.dependencies.push(dependency);
+        }
+        self
+    }
+
+    /// Applies a key to the injected service.
+    pub fn with_key<TKey: 'static>(mut self) -> Self {
+        self.key_type = Some(Type::of::<TKey>());
+        self
+    }
+
+    /// Indicates the injected service is mutable.
+    pub fn as_mut(mut self) -> Self {
+        self.activator.as_mut();
+        self
+    }
+
+    /// Builds and returns a new [service descriptor](struct.ServiceDescriptor.html).
+    pub fn build(mut self) -> ServiceDescriptor {
+        ServiceDescriptor::new(
+            self.lifetime,
+            if let Some(key) = self.key_type {
+                self.activator.service_type().with_key(&key)
+            } else {
+                self.activator.service_type().clone()
+            },
+            self.activator.implementation_type().clone(),
+            if self.dependencies.is_empty() {
+                Vec::with_capacity(0)
+            } else {
+                self.dependencies.shrink_to_fit();
+                self.dependencies
+            },
+            Once::new(),
+            self.activator.factory(),
+        )
+    }
+}
+
+impl From<InjectBuilder> for ServiceDescriptor {
+    fn from(value: InjectBuilder) -> Self {
+        value.build()
+    }
+}
