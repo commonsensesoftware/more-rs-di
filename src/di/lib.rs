44fb9a02ac06d0eb79fd1f79a96eb96bd0d4745b
Chris Martinez <sydefekt@hotmail.com>
2023-11-21 19:12:01 -0800

    Rename keyed_ref to keyed

--- src/di/lib.rs
+++ src/di/lib.rs
@@ -9,7 +9,7 @@ pub(crate) type Mut<T> = std::sync::RwLock<T>;
 mod collection;
 mod dependency;
 mod descriptor;
-mod keyed_ref;
+mod keyed;
 mod provider;
 mod r#type;
 mod validation;
@@ -38,7 +38,7 @@ mod test;
 pub use collection::*;
 pub use dependency::*;
 pub use descriptor::*;
-pub use keyed_ref::*;
+pub use keyed::*;
 pub use provider::*;
 pub use r#type::*;
 pub use validation::*;
@@ -64,11 +64,6 @@ pub use di_macros::{inject, injectable};
 /// Contains support for lazy service resolution.
 #[cfg(feature = "lazy")]
 pub mod lazy {
-    // use super::*;
     use super::lazy_init;
-    // pub use lazy_init::{
-    //     empty, empty_with_key, exactly_one, exactly_one_mut, exactly_one_with_key, missing, missing_with_key,
-    //     zero_or_more, zero_or_more_with_key, zero_or_one, zero_or_one_with_key, Lazy,
-    // };
     pub use lazy_init::*;
 }
