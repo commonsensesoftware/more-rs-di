1394f88aed2f0825cfd5f2be30a34753e63b974e
Chris Martinez <sydefekt@hotmail.com>
2026-03-02 10:43:36 -0800

    Fix and clean up doc tests

--- src/di/build.rs
+++ src/di/build.rs
@@ -6,10 +6,15 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn Error>> {
     let cwd = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap());
     let root = cwd.parent().unwrap().parent().unwrap();
-    let readme = root.join("README.md");
+    let src = root.join("README.md");
+    let dst = cwd.join("README.md");
 
-    if readme.exists() {
-        copy(root.join("README.md"), cwd.join("README.md"))?;
+    println!("cargo:rerun-if-changed={}", src.display());
+
+    if dst.exists() {
+        println!("cargo:rerun-if-changed={}", dst.display());
+    } else {
+        copy(src, dst)?;
     }
 
     Ok(())
